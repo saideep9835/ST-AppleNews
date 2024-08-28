7aeb3a7f60935ec5ffbd04bffde04e61b3778707
Saideep9835 <tsaideep98@gmail.com>
2024-08-28 11:10:16 -0500

    Initial Commit

---
+++ ST-AppleNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ST-AppleNews
+//
+//  Created by Saideep Reddy Talusani on 8/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ST-AppleNewsUITests/ST_AppleNewsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ST_AppleNewsUITestsLaunchTests.swift
+//  ST-AppleNewsUITests
+//
+//  Created by Saideep Reddy Talusani on 8/28/24.
+//
+
+import XCTest
+
+final class ST_AppleNewsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
